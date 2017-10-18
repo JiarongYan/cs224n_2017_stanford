10246af1bec64aebd613b1ae0972dc3e6b3ea355
JiarongYan <1583591142@qq.com>
2017-10-18 12:15:45 +0800

    q1_softmax.py

--- assignment1/q1_softmax.py
+++ assignment1/q1_softmax.py
@@ -31,12 +31,30 @@ def softmax(x):
     if len(x.shape) > 1:
         # Matrix
         ### YOUR CODE HERE
-        raise NotImplementedError
+        #Added by Jiarong
+#softmax(x) = softmax(x + c)
+#Note: In practice, we make use of this property and choose c = -maxi xi when computing softmax
+#probabilities for numerical stability (i.e., subtracting its maximum element from all elements of x)
+#so here: softmax(x) = softmax(x-maxi xi)
+
+        #axis = 1 means add by rows
+        #keepdims = True, explaination below
+        c = - np.max(x, axis = 1, keepdims=True)
+        x = x + c
+        
+        x = np.exp(x) / (np.sum(np.exp(x), axis = 1)).reshape(-1,1)
+
+        #raise NotImplementedError
         ### END YOUR CODE
     else:
         # Vector
         ### YOUR CODE HERE
-        raise NotImplementedError
+                
+        c = - np.max(x)
+        x = x + c
+        x = np.exp(x) / np.sum(np.exp(x))
+ 
+        #raise NotImplementedError
         ### END YOUR CODE
 
     assert x.shape == orig_shape
@@ -78,10 +96,54 @@ def test_softmax():
     """
     print "Running your tests..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+    #raise NotImplementedError
     ### END YOUR CODE
 
 
 if __name__ == "__main__":
     test_softmax_basic()
     test_softmax()
+
+    
+  
+'''
+Added by Jiarong
+explain: why use keepdims=True
+https://stackoverflow.com/questions/40927156/what-the-role-of-keepdims-in-python
+
+Consider a small 2d array:
+
+In [180]: A=np.arange(12).reshape(3,4)
+In [181]: A
+Out[181]: 
+array([[ 0,  1,  2,  3],
+       [ 4,  5,  6,  7],
+       [ 8,  9, 10, 11]])
+Sum across rows; the result is a (3,) array
+
+In [182]: A.sum(axis=1)
+Out[182]: array([ 6, 22, 38])
+But to sum (or divide) A by the sum requires reshaping
+
+In [183]: A-A.sum(axis=1)
+...
+ValueError: operands could not be broadcast together with shapes (3,4) (3,) 
+In [184]: A-A.sum(axis=1)[:,None]   # turn sum into (3,1)
+Out[184]: 
+array([[ -6,  -5,  -4,  -3],
+       [-18, -17, -16, -15],
+       [-30, -29, -28, -27]])
+If I use keepdims, "the result will broadcast correctly against" A.
+
+In [185]: A.sum(axis=1, keepdims=True)   # (3,1) array
+Out[185]: 
+array([[ 6],
+       [22],
+       [38]])
+In [186]: A-A.sum(axis=1, keepdims=True)
+Out[186]: 
+array([[ -6,  -5,  -4,  -3],
+       [-18, -17, -16, -15],
+       [-30, -29, -28, -27]])
+
+'''
